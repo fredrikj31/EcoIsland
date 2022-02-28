0f4be655099d82863ee8b8ae07d22eee7f1943b4
Fredrik Johansen <fredrik1206@gmail.com>
2022-02-28 08:28:03 +0100

    Added fence and outline tiles, and fixed click detection

--- Assets/Scripts/CropsController.cs
+++ Assets/Scripts/CropsController.cs
@@ -6,9 +6,14 @@ using UnityEngine.Tilemaps;
 
 public class CropsController : MonoBehaviour
 {
-
+	public Grid grid;
 	public Tilemap cropsMap;
-	public List<Crop> crops;
+	public List<Crop> crops = new List<Crop>();
+
+
+	// Time management
+     private float downClickTime;
+     private float ClickDeltaTime = 0.2F;
 
     // Start is called before the first frame update
     void Start()
@@ -16,14 +21,34 @@ public class CropsController : MonoBehaviour
 		InvokeRepeating("updateCropsTime", 0f, 1f);
     }
 
+	void Update() 
+	{
+		if (Input.GetMouseButtonDown(0)) {
+			this.downClickTime = Time.time;
+		}
+
+		if (Input.GetMouseButtonUp(0)) {
+			if(Time.time - downClickTime <= ClickDeltaTime) {
+            	Debug.Log(this.cropsMap.GetTile(this.getMousePosition()).name);
+			}
+		}
+	}
+
+	Vector3Int getMousePosition() {
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        return grid.WorldToCell(mouseWorldPos);
+    }
+
 	private void updateCropsTime() {
-		foreach (Crop crop in this.crops) {
-			// Calculate time
-			DateTime now = DateTime.Now;
+		if (this.crops.Count > 0) {	
+			foreach (Crop crop in this.crops) {
+				// Calculate time
+				DateTime now = DateTime.Now;
 
-			TimeSpan difference = now.Subtract(crop.plantedTime);
+				TimeSpan difference = now.Subtract(crop.plantedTime);
 
-			double procents = (difference.Minutes / crop.growTime) * 100;
+				double procents = (difference.Minutes / crop.growTime) * 100;
+			}
 		}
 	}
 
