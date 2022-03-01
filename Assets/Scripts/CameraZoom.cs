56fe08251ea103fa56689724740cbe8d31ad3c90
Fredrik Johansen <fredrik1206@gmail.com>
2022-03-01 13:38:55 +0100

    Added island and camera zoom

---
+++ Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+	public float maxZoomIn;
+	public float maxZoomOut;
+	public float zoomFactor;
+
+	private Camera mainCamera;
+
+    void Start() {
+		this.mainCamera = this.GetComponent<Camera>();
+	}
+
+	public void zoomIn() {
+		if (this.mainCamera.orthographicSize - this.zoomFactor >= this.maxZoomIn) {
+			this.mainCamera.orthographicSize = this.mainCamera.orthographicSize - this.zoomFactor;
+		} else {
+			Debug.Log("You can't zoom more in.");
+		}
+	}
+
+	public void zoomOut() {
+		if (this.mainCamera.orthographicSize + this.zoomFactor <= this.maxZoomOut) {
+			this.mainCamera.orthographicSize = this.mainCamera.orthographicSize + this.zoomFactor;
+		} else {
+			Debug.Log("You can't zoom more out.");
+		}
+	}
+}
