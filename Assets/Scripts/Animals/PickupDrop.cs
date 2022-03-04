2c0c4d40bca998848ac6f52aed9314380ad42259
Fredrik Johansen <fredrik1206@gmail.com>
2022-03-04 15:32:37 +0100

    Added file to pick up items

--- Assets/Scripts/Animals/PickupDrop.cs
+++ Assets/Scripts/Animals/PickupDrop.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class PickupDrop : MonoBehaviour
+public class PickupDrop : MonoBehaviour, IPointerClickHandler
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	//Detect if a click occurs
+	public void OnPointerClick(PointerEventData pointerEventData)
+	{
+		//Output to console the clicked GameObject's name and the following message. You can replace this with your own actions for when clicking the GameObject.
+		Debug.Log(name + " Game Object Clicked!");
+	}
 }
