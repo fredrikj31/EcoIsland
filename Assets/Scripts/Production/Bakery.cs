150765d0765584bde35db7fc766adc2098e26b6b
Fredrik Johansen <fredrik1206@gmail.com>
2022-03-31 14:24:24 +0200

    Last commit from laptop

--- Assets/Scripts/Production/Bakery.cs
+++ Assets/Scripts/Production/Bakery.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 namespace EcoIsland
@@ -7,30 +8,87 @@ namespace EcoIsland
 	public class Bakery : MonoBehaviour
 	{
 		public int bakeryStatus;
+		public GameObject barn;
+		private bool isFinished;
+		private BarnStorage storage;
 		private GameObject bakeryMenu;
 		private Animator animator;
+		private Bread bread;
 
 		// Start is called before the first frame update
 		void Start()
 		{
+			this.storage = this.barn.GetComponent<BarnStorage>();
 			this.bakeryMenu = GameObject.FindGameObjectWithTag("BakeryMenu").transform.GetChild(0).gameObject;
 			this.animator = this.GetComponent<Animator>();
+
+			// Updates time and so on...
+			InvokeRepeating("updateBreadTime", 0f, 1f);
 		}
 
 		// Update is called once per frame
 		void Update()
 		{
-			this.animator.SetInteger("bakeryStatus", this.bakeryStatus);
 		}
 
 		void OnMouseDown()
 		{
-			this.bakeryMenu.SetActive(true);
+			if (this.isFinished == true) {
+				this.storage.addItem(ItemTypes.Bread);
+			} else {
+				this.bakeryMenu.SetActive(true);
+			}
+		}
+
+		private void updateBreadTime() {
+			int stage = bread.checkTime();
+			if (stage == 2) {
+				this.isFinished = true;
+				this.bread = null;
+			} else if (stage == 1 || stage == 0) {
+				this.animator.SetInteger("bakeryStatus", 1);
+			} else {
+				this.animator.SetInteger("bakeryStatus", 0);
+			}
+		}
+		private string formatData(Bread data)
+		{
+			TimeSpan remainingTime = data.getRemainingTime();
+			string popupText = $"";
+
+			if (data.checkTime() == 2)
+			{
+				popupText = $"Finished";
+			}
+			else
+			{
+				if (remainingTime.Hours > 0)
+				{
+					popupText = $"{remainingTime.Hours} hrs {remainingTime.Minutes} min {remainingTime.Seconds} sec";
+				}
+				else if (remainingTime.Minutes > 0)
+				{
+					popupText = $"{remainingTime.Minutes} min {remainingTime.Seconds} sec";
+				}
+				else
+				{
+					popupText = $"{remainingTime.Seconds} sec";
+				}
+			}
+
+			return popupText;
 		}
 
 		public void closeMenu()
 		{
 			this.bakeryMenu.SetActive(false);
+		}
+
+		public void bakeBread() {
+			if (this.bread == null) {
+				Bread bread = new Bread(DateTime.Now);
+				this.bread = bread;
+			}
 		}
 	}
 }
---
+++ Assets/Types/Bread.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+namespace EcoIsland
+{
+	public class Bread : MonoBehaviour
+	{
+		public DateTime startedTime;
+		public int bakeTimeSeconds = 600;
+
+		public Bread(DateTime startedTime)
+		{
+			this.startedTime = startedTime;
+		}
+
+		// Check time
+		public int checkTime()
+		{
+			// Calculate time
+			DateTime now = DateTime.Now;
+			TimeSpan difference = now.Subtract(this.startedTime);
+			double procents = (difference.TotalSeconds / this.bakeTimeSeconds) * 100;
+
+			// Stages: (0 = level 1), (1 = level 2), (2 = level 3)
+			if (procents <= 50.0)
+			{
+				return 0;
+			}
+			else if (procents < 100.0)
+			{
+				return 1;
+			}
+			else
+			{
+				return 2;
+			}
+		}
+
+		public double getProcents()
+		{
+			// Calculate time
+			DateTime now = DateTime.Now;
+			TimeSpan difference = now.Subtract(this.startedTime);
+
+			double procents = (difference.TotalSeconds / this.bakeTimeSeconds) * 100;
+
+			return procents;
+		}
+
+		public TimeSpan getRemainingTime()
+		{
+			DateTime plantTime = this.startedTime;
+
+			DateTime finishedDate = plantTime.AddSeconds(this.bakeTimeSeconds);
+			TimeSpan remainingTime = finishedDate.Subtract(DateTime.Now);
+
+			return remainingTime;
+		}
+	}
+}
